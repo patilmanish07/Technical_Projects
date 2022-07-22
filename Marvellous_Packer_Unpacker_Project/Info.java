bc3c861d3d915c7705bf2e6564e3ab00253f350d
Manish patil <99861707+patilmanish07@users.noreply.github.com>
2022-07-22 11:54:03 +0530

    first commit

---
+++ Marvellous_Packer_Unpacker_Project/Info.java
@@ -0,0 +1,10 @@
+
+Main purpose of this project is to merge large amount of files into one file by avoiding memory wastage. We also provide data security by using the concept of Encryption. For next level data security we add MD5 Checksum check and Primary header check.
+
+//1) This Application Is Used To Perform Packing And Unpacking Activity For Multiple Types Of Files.
+
+//2) In Case Of Packing Activity I Maintain One File Which Contains Metadata And Data Of Multiple Files From Specified Directory.
+
+//3) In Case Of Unpacking Activity I Extract All Data From Packed Files And According To Its Metadata It Create All Files.
+
+//4) In This Application I Use Java As Frontend As Well As Backend Also For Platform Independency.
